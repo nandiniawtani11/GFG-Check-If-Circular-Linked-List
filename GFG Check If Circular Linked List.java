280ee989b74ac17e8fe312498ee031d9b67a1fbb
nandiniawtani11 <62164797+nandiniawtani11@users.noreply.github.com>
2021-07-21 16:46:31 +0530

    Create GFG Check If Circular Linked List.java

---
+++ GFG Check If Circular Linked List.java
@@ -0,0 +1,19 @@
+class GfG
+{
+    boolean isCircular(Node head)
+    {
+	// Your code here
+	Node fast=head;
+	Node slow=head;
+	while(fast!=null && fast.next!=null)
+	{
+	    fast=fast.next.next;
+	    slow =slow.next;
+	    if(fast==slow)
+	    {
+	        return true;
+	    }
+	}
+	return false;
+    }
+}
